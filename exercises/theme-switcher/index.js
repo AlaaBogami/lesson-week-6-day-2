3472bfe1c93e49dc9ed8bfc49cafa67c7ea9378d
A L A A <alaa.bogami93@gmail.com>
2019-03-05 18:45:53 +0300

    HW6 Day6
    I love this homwrok using JQuery it's really fun when we learn and try solve it
    thank you

--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,61 @@
-// click event for gray button
 
-// click event for white button
+//--------------------------------
 
-function switchGray() {
-    // change background to gray
-    // change font to white
+function switchGray(){
+
+  let switcher = $("#switcher");
+  // change background to gray
+  document.body.style.backgroundColor = 'gray'
+
+  // let body = $("body");
+  // body.css("background","gray")
+
+  // change font to white
+  $("p").css("color","white");
+  $("h1").css("color","white");
+
+  // body.css("color","white");
+
+
+  //or
+  // نجمعها كلها مع بعض
+  // body.css({"background":"gray",
+  // "color":"white"})
+
+
 }
+
+//---------------------------------
 
 function switchWhite() {
-    // change background to white
-    // change font to black
+
+  let switcher = $("#switcher");
+// change background to white
+document.body.style.backgroundColor = 'White'
+// let body = $("body");
+// body.css("background","white")
+
+// change font to black
+$("p").css("color","black");
+$("h1").css("color","black");
+
+  // body.css("color","black");
+
+
+//or
+// نجمعها كلها مع بعض
+// body.css({"background":"white",
+// "color":"black"})
+
 }
+//---------------------------------
+
+
+// click event for gray button
+// switchGray();
+$("#grayButton").on("click",switchGray);
+
+
+// click event for white button
+// switchWhite();
+$("#whiteButton").on("click",switchWhite);
